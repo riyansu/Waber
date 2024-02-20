9a5e3324e4c9b0f9fedf10e86386f237b0316d9d
Riyansu <kenKEN.DEM@gmail.com>
2024-02-20 20:10:29 +0900

    create welcome page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		screens: {
+			'@mb': '420px',
+			'@tb': '768px',
+			'@lt': '1024px',
+			'@pc': '1540px',
+			'@dp': '1920px',
+		}
 	},
 	plugins: [],
 }
